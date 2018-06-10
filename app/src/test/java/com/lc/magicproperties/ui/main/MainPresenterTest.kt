6e46298b73561f26759b171626a153b38de9250c
Luis <luiscsmcosta@gmail.com>
2018-06-11 00:18:17 +0100

    % changes MainPresenterTest to access json on assets.

--- app/src/test/java/com/lc/magicproperties/ui/main/MainPresenterTest.kt
+++ app/src/test/java/com/lc/magicproperties/ui/main/MainPresenterTest.kt
@@ -1,8 +1,15 @@
 package com.lc.magicproperties.ui.main
 
+import android.support.test.InstrumentationRegistry
+import com.google.gson.Gson
+import com.lc.magicproperties.model.daos.PropertiesDAO
 import junit.framework.TestCase
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.Reader
 
 class MainPresenterTest: TestCase() {
 
@@ -16,4 +23,12 @@ class MainPresenterTest: TestCase() {
     }
 
 
+    @Test
+    fun testProperties() {
+        val inputStream : InputStream = InstrumentationRegistry.getContext().assets.open("properties.json")
+
+        val reader : Reader = InputStreamReader(inputStream, "UTF-8")
+
+        mainPresenter.onGetProperties(Gson().fromJson(reader, PropertiesDAO::class.java))
+    }
 }
