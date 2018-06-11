dc488c3782f3225b74e4642acca92441ef673b02
Luis <luiscsmcosta@gmail.com>
2018-06-11 20:21:04 +0100

    % changes PropertiesMapperTest to include more tests.

--- app/src/androidTest/java/com/lc/magicproperties/PropertiesMapperTest.kt
+++ app/src/androidTest/java/com/lc/magicproperties/PropertiesMapperTest.kt
@@ -12,22 +12,47 @@ import java.io.InputStream
 import java.io.InputStreamReader
 import java.io.Reader
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class PropertiesMapperTest {
 
+    lateinit var inputStream: InputStream
+    lateinit var reader: Reader
+    lateinit var propertiesDTO: PropertiesDTO
+    lateinit var propertiesDAO: PropertiesDAO
+
+    @Before
+    fun init() {
+        inputStream = InstrumentationRegistry.getContext().resources.assets.open("properties.json")
+
+        reader = InputStreamReader(inputStream, "UTF-8")
+
+        propertiesDTO = Gson().fromJson(reader, PropertiesDTO::class.java)
+
+        propertiesDAO = Mappers.getMapper(PropertiesMapper::class.java).fromPropertiesDtoToDao(propertiesDTO)
+    }
+
     @Test
-    fun testPropertiesMapper() {
-        val inputStream: InputStream = InstrumentationRegistry.getContext().resources.assets.open("properties.json")
-
-        val reader: Reader = InputStreamReader(inputStream, "UTF-8")
-
-        val propertiesDTO: PropertiesDTO = Gson().fromJson(reader, PropertiesDTO::class.java)
-
-        val propertiesDAO: PropertiesDAO = Mappers.getMapper(PropertiesMapper::class.java).fromPropertiesDtoToDao(propertiesDTO)
-
+    fun testFilterDataMapper() {
         assertEquals(propertiesDTO.filterData.lowestPricePerNight.value, propertiesDAO.filterData.lowestPricePerNight.value)
+        assertEquals(propertiesDTO.filterData.lowestPricePerNight.currency, propertiesDAO.filterData.lowestPricePerNight.currency)
+        assertEquals(propertiesDTO.filterData.highestPricePerNight.value, propertiesDAO.filterData.highestPricePerNight.value)
+        assertEquals(propertiesDTO.filterData.highestPricePerNight.currency, propertiesDAO.filterData.highestPricePerNight.currency)
+    }
+
+    @Test
+    fun testLocationMapper() {
+        assertEquals(propertiesDTO.location.city.country, propertiesDAO.location.city.country)
+        assertEquals(propertiesDTO.location.city.id, propertiesDAO.location.city.id)
+        assertEquals(propertiesDTO.location.city.idCountry, propertiesDAO.location.city.idCountry)
+        assertEquals(propertiesDTO.location.city.name, propertiesDAO.location.city.name)
+        assertEquals(propertiesDTO.location.region, propertiesDAO.location.region)
+    }
+
+    @Test
+    fun testPropertiesList() {
         assertEquals(propertiesDTO.properties.size, propertiesDAO.properties.size) //passes
         assertEquals(propertiesDTO.properties.size, propertiesDAO.properties.size + 1) //fails
     }
